0a54a23446516c0c4350b77966d8ed8189767723
Phyo Thu Ya <vik8312@gmail.com>
2024-03-17 18:15:12 +0630

    update

--- src/components/Nav.Component.jsx
+++ src/components/Nav.Component.jsx
@@ -178,46 +178,25 @@ const NavComponent = () => {
 										</a>
 									</li>
 									<li>
-										<button
-											type="button"
-											className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-											aria-controls="dropdown-example"
-											data-collapse-toggle="dropdown-example">
+										<a
+											onClick={() => nav("/dashboard/collections")}
+											className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 											<svg
-												className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-												aria-hidden="true"
 												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 24 24"
 												fill="currentColor"
-												viewBox="0 0 18 21">
-												<path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-											</svg>
-											<span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
-												E-commerce
-											</span>
-											<svg
-												className="w-3 h-3"
-												aria-hidden="true"
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 10 6">
+												className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
 												<path
-													stroke="currentColor"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth="2"
-													d="m1 1 4 4 4-4"
+													fillRule="evenodd"
+													d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z"
+													clipRule="evenodd"
 												/>
 											</svg>
-										</button>
-										<ul id="dropdown-example" className="hidden py-2 space-y-2">
-											<li>
-												<a
-													onClick={() => nav("/dashboard/collections")}
-													className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-													Products
-												</a>
-											</li>
-										</ul>
+
+											<span className="flex-1 ms-3 whitespace-nowrap">
+												Collections
+											</span>
+										</a>
 									</li>
 
 									<li>
@@ -254,7 +233,9 @@ const NavComponent = () => {
 									</li>
 
 									<li>
-										<a onClick={handleLogout} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+										<a
+											onClick={handleLogout}
+											className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 											<svg
 												className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
 												aria-hidden="true"
